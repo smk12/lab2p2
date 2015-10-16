94d56a057c3a0299f7016b3f8d7f618af91d6f50
smk12 <smk12@email.arizona.edu>
2015-10-15 20:36:19 -0700

    Update main.c

--- main.c
+++ main.c
@@ -23,16 +23,22 @@
 
 
 typedef enum stateTypeEnum{
-    write, wait, debouncePress, debounceRelease
+    write, wait, debouncePress, debounceRelease, reset
 } stateType;
 
 volatile stateType state;
-volatile int xlcdPos;
-volatile int ylcdPos;
+volatile int inNum = 0;          //counts # of current inputs
+volatile char passIn[4][10];
+volatile int passX;
+volatile int passY;
+volatile char currIn[4];
+volatile int currX;        //First array in currIn
+volatile int setMode;      //0 when checking pass. 1 when setting pass
 volatile int temp;
 volatile int temp2;
 volatile int temp3;
 volatile char keyScan;
+volatile int loop;
 
 
 int main(void)
@@ -41,14 +47,22 @@ int main(void)
     enableInterrupts();
     initKeypad();
     initLCD();
-    state = wait;
-    xlcdPos = 0;
-    ylcdPos = 0;
+    state = reset;
+    currX = 0;
+    setMode = 0;
     
     while(1)
     {
         switch(state)
         {
+            case reset:
+                clearLCD();
+                printStringLCD("Enter");
+                moveCursorLCD(1,0);
+                inNum = 0;
+                setMode = 0;
+                state = wait;
+                break;
             case wait:
                 
                 break;
@@ -59,19 +73,99 @@ int main(void)
                     state = wait;
                     break;
                 }
-                else
+                else if(keyScan==0x23)
                 {
-                    printCharLCD(keyScan);
-                    xlcdPos++;
-                    if(xlcdPos==8)
+                    clearLCD();
+                    moveCursorLCD(0,0);
+                    printStringLCD("Bad");
+                    for(loop=0;loop<=20;loop++)
+                        delayMs(200);
+                    currX = 0;
+                    state = reset;
+                    break;
+                }
+                else if(keyScan==0x2a)
+                {
+                    if((currX==0)&(setMode==0))
                     {
-                        xlcdPos = 0;
-                        ylcdPos++;
-                        if(ylcdPos==2)
-                            ylcdPos=0;
-                        moveCursorLCD(ylcdPos, xlcdPos);
+                        currIn[0] = 0x2a;
+                        printCharLCD(0x2a);
+                        state = wait;
+                    }
+                    else if((currIn[0] == 0x2a)&(currX==1)&(setMode==0))
+                    {
+                        clearLCD();
+                        moveCursorLCD(0,0);
+                        printStringLCD("Set Mode");
+                        moveCursorLCD(1,0);
+                        setMode = 1;
+                        currX = 0;
+                        state = wait;
+                        break;
+                    }
+                    else
+                    {
+                        clearLCD();
+                        moveCursorLCD(0,0);
+                        printStringLCD("Invalid");
+                        for(loop=0;loop<=20;loop++)
+                            delayMs(200);
+                        currX = 0;
+                        setMode = 0;
+                        state = reset;
+                        break;
                     }
                 }
+                else if(setMode==0)
+                {
+                    currIn[currX] = keyScan;
+                    printCharLCD(keyScan);
+                }
+                else if(setMode==1)
+                {
+                    passIn[passX][passY] = keyScan;
+                    printCharLCD(keyScan);
+                }
+                else
+                {
+                    clearLCD();
+                    moveCursorLCD(0,0);
+                    printStringLCD("Error");
+                    for(loop=0;loop<=20;loop++)
+                        delayMs(200);
+                    currX = 0;
+                    setMode = 0;
+                    state = reset;
+                }
+                if((currX==3)&(setMode==0))
+                {
+                    
+                    //check if valid password
+                    clearLCD();
+                    moveCursorLCD(0,0);
+                    printStringLCD("Bacon");
+                    for(loop=0;loop<=20;loop++)
+                        delayMs(200);
+                    currX = 0;
+                    setMode = 0;
+                    state = reset;
+                    break;
+                }
+                else if((currX==3)&(setMode==1))
+                {
+                    clearLCD();
+                    moveCursorLCD(0,0);
+                    printStringLCD("Valid");
+                    for(loop=0;loop<=20;loop++)
+                        delayMs(200);
+                    currX = 0;
+                    setMode = 0;
+                    state = reset;
+                    passX = 0;
+                    passY++;
+                    break;
+                }
+                currX++;
                 state = wait;
                 break;
             case debouncePress:
@@ -95,7 +189,10 @@ void __ISR(_CHANGE_NOTICE_VECTOR, IPL7SRS) _CNInterrupt(void)
     IFS1bits.CNGIF = 0;
     temp = PORT_0;temp2 = PORT_1;temp3 = PORT_2;
     if ((temp==RELEASE)&(temp2==RELEASE)&(temp3==RELEASE))
-        state = debounceRelease;
+    {
+        //state = debounceRelease;
+        //delayMs(5);
+    }
     else
         state = debouncePress;
     
